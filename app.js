e55caceb4d16a4dd2684e429c1435f4f75dc5b20
Abdirahman Suldaan <suldaan.89@hotmail.com>
2023-04-05 01:16:16 +0300

    fix breaking changes

--- app.js
+++ app.js
@@ -7,14 +7,11 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  throw 'error'
-
-  // eslint-disable-next-line no-unreachable
   res.send('ok')
 })
 
 app.get('/version', (req, res) => {
-  res.send('v5')
+  res.send('v7')
 })
 
 app.listen(PORT, () => {
